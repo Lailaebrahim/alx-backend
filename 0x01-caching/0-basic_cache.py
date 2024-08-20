8f91d74a690bf5c6b80be7d2144c5d4cc8039387
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-21 00:18:29 +0300

    Task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,18 +5,17 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """_summary_
+	"""_summary_
     class BasicCache that inherits from BaseCaching and is a caching system
     Args:
         BaseCaching (_type_): _description_
     """
+	def put(self, key, item):
+		if key is None or item is None:
+			return
+		self.cache_data[key] = item
 
-    def put(self, key, item):
-      if key is None or item is None:
-        return
-      self.cache_data[key] = item
-      
-    def get(self, key):
-      if key is None or key not in self.cache_data:
-        return None
-      return self.cache_data[key]
+	def get(self, key):
+		if key is None or key not in self.cache_data:
+			return None
+		return self.cache_data[key]
