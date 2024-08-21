5d85aab2a91ce74b1820e165abdc19cc67cebe50
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-22 02:28:31 +0300

    Task 3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Class LRUCache that inherits from BaseCaching
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """
+    Class FIFOCache that inherits from BaseCaching and is a caching system
+    """
+    def __init__(self):
+        """
+        Initializes the LRUCache instance
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Add an item to the cache.
+        Args:
+            key (any): The key to associate with the item.
+            item (any): The item to be added to the cache.
+        """
+        if key is None or item is None:
+            return
+        elif len(self.cache_data) >= self.MAX_ITEMS:
+            key_to_remove,_ = self.cache_data.popitem(True)
+            print("DISCARD: {}".format(key_to_remove))
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=False)
+        
+
+    def get(self, key):
+        """
+        Retrieve item from cache
+        Args:
+            key (any): The key to associate with the item.
+        Returns:
+            item(any): item associate with the key.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.cache_data.move_to_end(key, last=False)
+        return self.cache_data[key]
