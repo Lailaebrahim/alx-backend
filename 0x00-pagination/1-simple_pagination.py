2c9c0e28e31fdac6086ea690478d71974e37c1aa
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-24 01:47:48 +0300

    Task 1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+""" a function named index_range that takes two integer args page and page_size
+"""
+from typing import Tuple, List
+import csv
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    The function should return a tuple of size two containing
+    a start index and an end index corresponding
+    to the range of indexes to return in a list for
+    those particular pagination parameters.
+
+    Args:
+        page (int): _description_
+        page_size (int): _description_
+
+    Returns:
+        tuple: _description_
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """_summary_
+
+        Args:
+            page (int, optional): _description_. Defaults to 1.
+            page_size (int, optional): _description_. Defaults to 10.
+
+        Returns:
+            List[List]: _description_
+        """
+        assert isinstance(
+            page_size, int) and isinstance(page, int)
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
+        page_req = dataset[start:end]
+        return page_req
+ 
