6ff417fc24791f9e9387a2d8ac063cd438734c3c
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-24 00:42:39 +0300

    Task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -18,4 +18,6 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     Returns:
         tuple: _description_
     """
-    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
